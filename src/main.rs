c549026710157039eccdb29a2fa03972a6b74722
Leandro Guedes <leandroguedes@protonmail.com>
2021-03-10 20:20:51 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate anyhow;
+extern crate grrs;
+extern crate structopt;
+
+use anyhow::{Context, Result};
+use grrs::find_matches;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    find_matches(&content, &args.pattern);
+    Ok(())
+}
